f5e019069e1c90853bad337aafb9349242ea6072
NikTishkov <nik.tishkov25@gmail.com>
2025-04-02 19:22:23 +0700

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
     public class Main {
     public static void main(String[] args) {
-        byte machinePerformance = 16;
-        int machinePerformance1 = machinePerformance * 10;
-        int machinePerformance2 = machinePerformance * 30 * 24;
-        int machinePerformance3 = machinePerformance2 * 3;
-        int machinePerformance4 = machinePerformance2 * 31;
-        System.out.println("За 20 минут машина произвела "+machinePerformance1+" штук бутылок.");
-        System.out.println("За сутки машина произвела "+machinePerformance2+" штук бутылок.");
-        System.out.println("За 3 дня машина произвела "+machinePerformance3+" штук бутылок.");
-        System.out.println("За 1 месяц машина произвела "+machinePerformance4+" штук бутылок.");
+        byte totalCans = 120;
+        byte whitePaint = 2;
+        byte brownPaint = 4;
+        int totalClasses = totalCans / (whitePaint+brownPaint);
+        int totalWhitePaint = totalClasses * whitePaint;
+        int totalBrownPaint = totalClasses * brownPaint;
+        System.out.println("В школе, где "+totalClasses+" классов, нужно "+totalWhitePaint+" банок белой краски и "+totalBrownPaint+" банок коричневой краски.");
     }
 }
