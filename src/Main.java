e0c8cfc5ce903efe5d5eb18eb7fdeb94922f6570
NikTishkov <nik.tishkov25@gmail.com>
2025-04-02 18:27:56 +0700

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
     public class Main {
     public static void main(String[] args) {
-        byte first = -126;
-        short second = 32560;
-        int third = 50000;
-        long fourth = 100000L;
-        float i = 65.1298465f;
-        double g = 85.989435943;
-        System.out.println("Значение переменной first с типом byte равно "+first);
-        System.out.println("Значение переменной second с типом short равно "+second);
-        System.out.println("Значение переменной third с типом int равно "+third);
-        System.out.println("Значение переменной fourth с типом long равно "+fourth);
-        System.out.println("Значение переменной i с типом float равно "+i);
-        System.out.println("Значение переменной g с типом double равно "+g);
+        float g = 2.786f;
+        long a = 987678965549L;
+        float i = 2.786f;
+        short k = 569;
+        short b = -159;
+        short u = 27897;
+        byte p = 67;
+        System.out.println(g);
+        System.out.println(a);
+        System.out.println(i);
+        System.out.println(k);
+        System.out.println(b);
+        System.out.println(u);
+        System.out.println(p);
     }
 }
