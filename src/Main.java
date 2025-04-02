320e29c9afdd38c4086063efa623e98975af131a
NikTishkov <nik.tishkov25@gmail.com>
2025-04-02 18:49:51 +0700

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
     public class Main {
     public static void main(String[] args) {
-        float g = 2.786f;
-        long a = 987678965549L;
-        float i = 2.786f;
-        short k = 569;
-        short b = -159;
-        short u = 27897;
-        byte p = 67;
-        System.out.println(g);
-        System.out.println(a);
-        System.out.println(i);
-        System.out.println(k);
-        System.out.println(b);
-        System.out.println(u);
-        System.out.println(p);
+        byte teacher1 = 23;
+        byte teacher2 = 27;
+        byte teacher3 = 30;
+        short sheetsOfPaper = 480;
+        int sheetsOfPaperPerPerson = sheetsOfPaper / (teacher1 + teacher2 + teacher3);
+        System.out.println("На каждого ученика расчитано "+sheetsOfPaperPerPerson+" листов бумаги.");
     }
 }
