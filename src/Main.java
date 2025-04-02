ad4e845f314896f15e514ca58caa02e963792938
NikTishkov <nik.tishkov25@gmail.com>
2025-04-02 19:58:36 +0700

    Task 6

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
     public class Main {
     public static void main(String[] args) {
-        byte totalCans = 120;
-        byte whitePaint = 2;
-        byte brownPaint = 4;
-        int totalClasses = totalCans / (whitePaint+brownPaint);
-        int totalWhitePaint = totalClasses * whitePaint;
-        int totalBrownPaint = totalClasses * brownPaint;
-        System.out.println("В школе, где "+totalClasses+" классов, нужно "+totalWhitePaint+" банок белой краски и "+totalBrownPaint+" банок коричневой краски.");
+        int bananasGr = 5 * 80;
+        int milkGr = 2 * 105;
+        int iceCreamGr = 2 * 100;
+        int eggsGr = 4 * 70;
+        int weightSportBreakfastGr = bananasGr + milkGr + iceCreamGr + eggsGr;
+        float weightSportBreakfastKg = weightSportBreakfastGr * 0.001f;
+        System.out.println("Вес спортзавтрака в граммах "+weightSportBreakfastGr);
+        System.out.println("Вес спортзавтрака в килограммах "+weightSportBreakfastKg);
     }
 }
